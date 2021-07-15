a08b3645ac232fa9044be0e5326964b6f0418ca1
kikap <kikap@kikapui-MacBookPro.local>
2021-07-15 13:46:09 +0900

    refs

--- src/components/Ref.vue
+++ src/components/Ref.vue
@@ -1,11 +1,36 @@
 <template>
     <div>
         ref
+        <input ref="name" name="name" placeholder="이름">
+        <input ref="age" name="age" placeholder="나이">
+        <input ref="job" name="job" placeholder="직업">
+        <input type="button" v-on:click="go" value="확인">
     </div>
 </template>
 
 <script>
 export default {
     name: 'ref-component',
+    methods: {
+        go: function() {
+            if (
+                this.checkInput(this.$refs.name)&&
+                this.checkInput(this.$refs.age) &&
+                this.checkInput(this.$refs.job)                 
+            ) {
+                alert("모두 입력되었습니다.")
+            }
+        },
+        checkInput: function(ref) {
+            if (ref.value.length <= 0) {
+                ref.focus()                    
+                return false
+            }
+            return true;
+        },
+    },
+    mounted() {
+        this.go()
+    }
 }
 </script>
