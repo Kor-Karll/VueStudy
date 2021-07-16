31d70c1cf3a8ee6fb677df35b41adc5a2a7f65e8
kikap <kikap@kikapui-MacBookPro.local>
2021-07-16 10:21:23 +0900

    mixin

--- src/components/Mixin.vue
+++ src/components/Mixin.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
         mixin
+        <div>
+            <img :src="this.brandi.imgSrc">
+        </div>
+        <div>
+            <span>{{this.brandi.titleData}}</span>
+        </div>
+        <div>
+            <span>{{this.brandi.text}}</span>
+        </div>
     </div>
 </template>
 
 <script>
+import Mixin from '../mixin/mixin'
 export default {
     name: 'mixin-component',
+    mixins: [Mixin],
+    mounted() {
+        this.cardClick()
+    }
 }
 </script>
