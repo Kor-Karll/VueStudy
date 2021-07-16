40928d8d07ce520f279941d01d5da4661ca73e5c
kikap <kikap@kikapui-MacBookPro.local>
2021-07-16 13:18:52 +0900

    Event

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,11 +1,39 @@
 <template>
     <div>
-        event
+        <div style="float:left;width:50%;">
+            <input v-on:keydown="keyDown">
+            <div v-for="item in keyItems">
+                {{item}}
+            </div>
+        </div>
+        <div style="float:left;width:50%;">
+            <div @mousemove="mouseMove" style="height:200px;border:1px solid black"></div>
+            <span>{{pageX}}, {{pageY}}</span>
+        </div>
     </div>
 </template>
-
 <script>
 export default {
     name: 'event-component',
+    methods: {
+        keyDown: function(e) {
+            if (e.keyCode == 8) {
+                this.keyItems.pop();
+                return;
+            }
+            this.keyItems.push(e.keyCode);
+        },
+        mouseMove: function (e) {
+            this.pageX = e.pageX
+            this.pageY = e.pageY
+        }
+    },
+    data() {
+        return {
+            keyItems: [],
+            pageX: 0,
+            pageY: 0,
+        }
+    }
 }
 </script>
