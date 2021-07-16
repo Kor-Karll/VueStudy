2a3289fd2a16f8be1f3494bbe5edc868a69433d0
kikap <kikap@kikapui-MacBookPro.local>
2021-07-16 15:34:14 +0900

    Axios

--- src/components/Axios.vue
+++ src/components/Axios.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
         axios
+        <table>
+            <tr v-for="product in productList">
+                <td>{{product.no}}</td>
+                <td>{{product.productName}}</td>
+                <td>{{product.price}}</td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
+import Axios from 'axios';
 export default {
     name: 'axios-component',
+    data() {
+        return {
+            productList: []
+        }
+    },
+    mounted() {
+        Axios.get('public/data.json').then(res => {
+            this.productList = res.data
+        })
+    }
 }
 </script>
